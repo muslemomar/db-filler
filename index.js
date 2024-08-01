dbb4c04a74162989d7f2e2f4edcc7996d53efc89
Muslim Omar <mislimumer@gmail.com>
2024-08-01 19:12:18 +0300

    second intial

--- index.js
+++ index.js
@@ -1,35 +1,52 @@
-const mysql = require('mysql2/promise');
-const {faker} = require('@faker-js/faker');
-const {argv} = require('node:process');
-const pg = require('pg');
+import mysql from 'mysql2/promise';
+import {faker} from '@faker-js/faker';
+import pg from 'pg';
+import inquirer from 'inquirer';
+import chalk from 'chalk';
 
+// TODO: add typescript
 // TODO: Duplicate entry bug. Check articletags table.
 // TODO (feature): validate column.
-// TODO: when database type is selected and connected, list all databases and ask user to select one.
 
-const dbType = argv[3];
+const IS_DEBUG_MODE = true;
 
-const connectToDb = async () => {
+const isPostgreSQLDb = () => dbType === 'pg';
 
-    let connection;
-
-    if (dbType === 'pg') {
-        const {Client} = pg
-        connection = new Client()
-        await connection.connect()
-    } else {
-        connection = await mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            database: 'db_auto_filler',
-            password: '', // test ide
-        });
-
-        const isDebug = false;
-        if (isDebug) attachDbLogger(connection);
+class Log {
+    static info(...msgs) {
+        console.log(chalk.blue(...msgs));
     }
 
-    console.log('Database connection established!');
+    static success(...msgs) {
+        console.log(chalk.green(...msgs));
+    }
+
+    static error(...msgs) {
+        console.log(chalk.red(...msgs));
+    }
+
+}
+
+const connectToDb = async (dbType, credentials) => {
+
+    Log.info('Connecting to database...');
+    let connection;
+
+    if (isPostgreSQLDb()) {
+        connection = new pg.Client(credentials)
+        await connection.connect()
+    } else {
+        connection = await mysql.createConnection(credentials);
+        if (IS_DEBUG_MODE) attachDbLogger(connection);
+    }
+
+    const originalQuery = connection.query;
+    connection.query = async (...args) => {
+        const queryResult = await originalQuery.call(connection, ...args);
+        return isPostgreSQLDb() ? queryResult.rows : queryResult[0];
+    }
+
+    Log.success('Database connection established!');
 
     return connection;
 }
@@ -57,8 +74,18 @@ const arrayFromRawEnumVals = (rawEnumValues) => {
     return rawEnumValues.match(/enum\((.*)\)/)[1].split(',').map(value => value.replaceAll('\'', ''));
 }
 
+const getUserDatabaseNames = async (connection) => {
+    const results = await connection.query("SELECT `schema_name` from INFORMATION_SCHEMA.SCHEMATA  WHERE `schema_name` NOT IN('information_schema', 'mysql', 'performance_schema', 'sys');");
+    const databaseNames = [];
+    for (const row of results) {
+        databaseNames.push(getValFromObject(row));
+    }
+
+    return databaseNames;
+}
+
 const getTableNames = async (connection) => {
-    const [results] = await connection.query('SHOW TABLES');
+    const results = await connection.query('SHOW TABLES');
     const tableNames = [];
     for (const row of results) {
         tableNames.push(getValFromObject(row));
@@ -67,32 +94,37 @@ const getTableNames = async (connection) => {
     return tableNames;
 }
 
-const DATABASE_NAME = 'db_auto_filler';
-
 const getTableColumns = async (connection, tableName, options = {}) => {
-    const [results] = await connection.query(`select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='${tableName}'`);
-
+    const results = await connection.query(`select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='${tableName}' AND TABLE_SCHEMA='${databaseName}'`);
     const columns = [];
     for (const row of results) {
-        const {COLUMN_NAME, DATA_TYPE, IS_NULLABLE, CHARACTER_MAXIMUM_LENGTH, EXTRA, TABLE_SCHEMA, COLUMN_TYPE} = row;
+
+
+        const columnName = isPostgreSQLDb() ? row.column_name : row.COLUMN_NAME;
+        const dataType = isPostgreSQLDb() ? row.udt_name : row.DATA_TYPE;
+        const isNullable = isPostgreSQLDb() ? row.is_nullable : row.IS_NULLABLE;
+        const maxLength = isPostgreSQLDb() ? row.character_maximum_length : row.CHARACTER_MAXIMUM_LENGTH;
+        const columnDefault = isPostgreSQLDb() ? row.column_default : row.EXTRA;
+        const columnType = isPostgreSQLDb() ? row.data_type : row.COLUMN_TYPE;
+        const isUnique = isPostgreSQLDb() ? row.is_unique : row.COLUMN_KEY === 'UNI';
 
         const validColumnValues = [];
-        if (COLUMN_TYPE.includes('enum')) {
-            validColumnValues.push(...arrayFromRawEnumVals(COLUMN_TYPE));
+        if (columnType.includes('enum')) {
+            validColumnValues.push(...arrayFromRawEnumVals(columnType));
         }
 
-        const isColumnAutoIncrement = EXTRA === 'auto_increment';
+        const isColumnAutoIncrement = !!columnDefault;
 
-        if (TABLE_SCHEMA !== DATABASE_NAME) continue;
         if (options.noAutoIncrement && isColumnAutoIncrement) continue;
 
         columns.push({
-            name: COLUMN_NAME,
-            type: DATA_TYPE,
-            isNullable: IS_NULLABLE === 'YES',
-            maxLength: CHARACTER_MAXIMUM_LENGTH,
-            isAutoIncrement: EXTRA === 'auto_increment',
-            validColumnValues
+            name: columnName,
+            type: dataType,
+            isNullable: isNullable === 'YES',
+            maxLength: maxLength,
+            isAutoIncrement: !!columnDefault,
+            validColumnValues,
+            isUnique
         });
     }
     return columns;
@@ -148,13 +180,11 @@ const getFakerFnByColumn = (columnName, columnType, validValues) => {
 }
 
 const constructInsertQuery = (tableName, columnNames) => {
-
     const columnsAsPlaceholders = columnNames.map(_ => '?').join(', ')
     const valuesPlaceholders = `${[...new Array(rowsToInsert)].map(_ => `(${columnsAsPlaceholders})`).join(', ')}`;
 
-    const insertQuery = `
-            INSERT INTO ${tableName} (${columnNames.join(', ')})
-            VALUES ${valuesPlaceholders}`;
+    /*TODO: remove IGNORE and instead find a way to use unique value always.*/
+    const insertQuery = `INSERT IGNORE INTO ${tableName} (${columnNames.join(', ')}) VALUES ${valuesPlaceholders}`;
 
     return insertQuery;
 }
@@ -175,28 +205,57 @@ const insertFakeDataIntoTable = async (connection, tableName, columns, relationa
             }
 
             const fakerMatchingFn = getFakerFnByColumn(column.name, column.type, column.validColumnValues);
-            insertValues.push(fakerMatchingFn());
+            insertValues.push(fakerMatchingFn()); // put a random value.
         }
     }
 
-    const [results] = await connection.execute(insertQuery, insertValues);
-    return results;
+    // TODO: possible bug
+    // if any of the "insertvalues" is undefined, change to null. TypeError: Bind parameters must not contain undefined. To pass SQL NULL specify JS null
+    /*
+    console.log('error of insertvalues undefined', err);
+        const index = insertValues.findIndex(x => x == null);
+        console.log('index', index);
+        console.log(insertQuery.slice(0, 50));
+        console.log(insertValues.slice(index - 5, index + 5));*/
+
+    return await connection.execute(insertQuery, insertValues);
 }
 
 const getTablesWithNoReferences = async (connection) => {
-    const [results] = await connection.query(`
+    let query = '';
+    if (isPostgreSQLDb()) {
+        query = `
+        SELECT TABLE_NAME
+        FROM information_schema.tables
+        WHERE table_schema = 'public'
+          AND TABLE_NAME NOT IN
+        (SELECT DISTINCT TABLE_NAME
+         FROM information_schema.key_column_usage
+         WHERE table_schema = 'public'
+       AND CONSTRAINT_NAME IN
+         (SELECT CONSTRAINT_NAME
+          FROM information_schema.table_constraints
+          WHERE constraint_type = 'FOREIGN KEY'
+            AND table_schema = 'public'
+ ) );
+        `
+    } else {
+        query = `
         SELECT table_name
         FROM information_schema.tables
-        WHERE table_schema = '${DATABASE_NAME}'
+        WHERE table_schema = '${databaseName}'
         AND table_name NOT IN (
             SELECT table_name
             FROM information_schema.key_column_usage
-            WHERE table_schema = '${DATABASE_NAME}'
+            WHERE table_schema = '${databaseName}'
             AND referenced_table_name IS NOT NULL
         )
-    `);
+    `;
+    }
 
-    const tableNames = []; // test ide
+    const results = await connection.query(query);
+
+    const tableNames = [];
     for (const row of results) {
         tableNames.push(getValFromObject(row));
     }
@@ -207,9 +266,8 @@ const getTablesWithNoReferences = async (connection) => {
 const fetchAllTablesRefs = async (connection) => {
 
     let query;
-    let results;
 
-    if (dbType === 'pg') {
+    if (isPostgreSQLDb()) {
         query = `SELECT tc.table_name AS child_table,
                    kcu.column_name AS child_column,
                    ccu.table_name AS parent_table,
@@ -226,30 +284,26 @@ const fetchAllTablesRefs = async (connection) => {
         query = `
         SELECT table_name, column_name, referenced_table_name, referenced_column_name
         FROM information_schema.key_column_usage
-        WHERE table_schema = '${DATABASE_NAME}'
+        WHERE table_schema = '${databaseName}'
         AND referenced_table_name IS NOT NULL`;
     }
 
-    // TODO: later make a function to encapsulate the query and results, so we don't care about handling the results, we just query and it returns the results no matter the db type.
+    const results = await connection.query(query);
 
-    // const [results] = await connection.query(query);
-    const queryResult = await connection.query(query);
-    if (dbType === 'pg') {
-        results = queryResult.fields;
-    } else {
-
-    }
-    console.log(x);
-    return;
     const tables = {};
     for (const row of results) {
-        if (!tables[row.TABLE_NAME]) {
-            tables[row.TABLE_NAME] = [];
+        const TABLE_NAME_KEY = isPostgreSQLDb() ? 'child_table' : 'TABLE_NAME';
+        const COLUMN_NAME_KEY = isPostgreSQLDb() ? 'child_column' : 'COLUMN_NAME';
+        const REFERENCED_TABLE_NAME_KEY = isPostgreSQLDb() ? 'parent_table' : 'REFERENCED_TABLE_NAME';
+        const REFERENCED_COLUMN_NAME_KEY = isPostgreSQLDb() ? 'parent_column' : 'REFERENCED_COLUMN_NAME';
+
+        if (!tables[row[TABLE_NAME_KEY]]) {
+            tables[row[TABLE_NAME_KEY]] = [];
         }
-        tables[row.TABLE_NAME].push({
-            column: row.COLUMN_NAME,
-            refTable: row.REFERENCED_TABLE_NAME,
-            refColumn: row.REFERENCED_COLUMN_NAME
+        tables[row[TABLE_NAME_KEY]].push({
+            column: row[COLUMN_NAME_KEY],
+            refTable: row[REFERENCED_TABLE_NAME_KEY],
+            refColumn: row[REFERENCED_COLUMN_NAME_KEY]
         });
     }
 
@@ -286,16 +340,8 @@ const populateTablesWithRefs = async (connection, tables, tablesWithRefs, popula
 
         const relationalData = {};
         for (const ref of tablesWithRefs[table]) {
-            const [results] = await connection.query(`select ${ref.refColumn} from ${ref.refTable} limit ${rowsToInsert}`);
-
-            /*if (table === 'articletags') {
-                console.log(`select ${ref.refColumn} from ${ref.refTable} ORDER BY RAND() limit ${LIMIT}`);
-            }*/
-            console.log('-----------------------');
-            console.log(ref);
-            console.log(results);
-            relationalData[ref.column] = results.map(row => getValFromObject(row)); // I'm only passing one value, pass all of them.
-            console.log(relationalData);
+            const results = await connection.query(`select ${ref.refColumn} from ${ref.refTable} limit ${rowsToInsert}`);
+            relationalData[ref.column] = results.map(row => getValFromObject(row));
         }
 
         const columns = await getTableColumns(connection, table, {noAutoIncrement: true});
@@ -327,7 +373,66 @@ const populateAllTables = async (connection) => {
 
 const closeDbConnection = (connection) => {
     connection.end();
-    console.log('Database connection closed!');
+}
+
+const promptInitialQuestions = async () => {
+    const questions = [
+        {
+            type: 'list',
+            name: 'databaseType',
+            message: 'What database do you want to use?',
+            choices: ['MySQL', 'PostgreSQL'],
+            filter(val) {
+                return val.toLowerCase();
+            },
+        },
+        {
+            type: 'input',
+            name: 'rowsToInsert',
+            message: 'How many rows do you want to insert per table?',
+            validate(value) {
+                const valid = value >= 1;
+                return valid || 'Please enter a valid number (>= 1)';
+            },
+            filter(val) {
+                if (isNaN(Number(val))) {
+                    return val;
+                }
+                return Number(val);
+            }
+        },
+        {
+            type: 'input',
+            name: 'host',
+            message: 'Enter the database host:',
+            default: 'localhost'
+        },
+        {
+            type: 'input',
+            name: 'user',
+            message: 'Enter the database user:',
+            default: 'root'
+        },
+        {
+            type: 'password',
+            name: 'password',
+            message: 'Enter the database password:',
+            mask: '*'
+        },
+    ]
+
+    return inquirer.prompt(questions);
+}
+
+
+const useDatabase = async (connection, databaseName) => {
+    await connection.query(`USE ${databaseName}`);
+}
+
+const selectDatabase = async (connection) => {
+    const databaseName = await promptSelectDatabase(connection);
+    global.databaseName = databaseName;
+    await useDatabase(connection, databaseName);
 }
 
 /*faker.word.sample({
@@ -337,17 +442,75 @@ const closeDbConnection = (connection) => {
 /*faker.lorem.paragraphs*/
 // also define number of rows to insert in all tables.
 
-(async () => {
+const promptSelectDatabase = async (connection) => {
+    const databaseNames = await getUserDatabaseNames(connection);
+    const questions = [
+        {
+            type: 'list',
+            name: 'databaseName',
+            message: 'Select a database to fill:',
+            choices: databaseNames,
+        }
+    ]
 
-    const connection = await connectToDb();
-    global.rowsToInsert = Number(argv[2]);
+    const answers = await inquirer.prompt(questions);
+    return answers.databaseName;
+}
+
+const getDatabaseCredentials = async () => {
+
+    const credentials = {};
+
+    if (isDebugMode) {
+        global.dbType = 'mysql';
+        global.databaseName = 'db_auto_filler';
+        global.dbType = 'mysql';
+        global.rowsToInsert = Number(process.argv[2]) || 1;
+        credentials.host = 'localhost';
+        credentials.user = 'root';
+        credentials.password = 'toor';
+        credentials.database = 'db_auto_filler';
+    } else {
+        const {databaseType, rowsToInsert, host, user, password} = await promptInitialQuestions();
+        global.dbType = databaseType;
+        global.rowsToInsert = rowsToInsert;
+        credentials.host = host;
+        credentials.user = user;
+        credentials.password = password;
+    }
+
+    return credentials;
+}
+
+global.isDebugMode = false;
+
+(async () => {
+    /*
+    * Setup global variables for debugging.
+    * TODO: remove after testing.
+    * */
+
+    const credentials = await getDatabaseCredentials();
+
+    const connection = await connectToDb(dbType, credentials);
+
+    if (!isDebugMode) {
+        await selectDatabase(connection);
+    }
+
+    Log.info('You have selected:');
+    Log.info(`Database Type: ${dbType}`);
+    Log.info(`Database name: ${databaseName}`);
+    Log.info(`Rows to insert: ${rowsToInsert}`);
 
     const populatedTables = await populateAllTables(connection);
     const totalTableNames = await getTableNames(connection);
 
     closeDbConnection(connection);
 
-    console.log('Populated tables:', populatedTables);
-    console.log(`Populated ${populatedTables.length} out of ${totalTableNames.length} tables`);
-    console.log('Database filled successfully! ✅️');
-})();
+    Log.info('Populated tables:', populatedTables);
+    Log.info(`Populated ${populatedTables.length} out of ${totalTableNames.length} tables`);
+    Log.success('Database filled successfully! ✅️');
+})().catch((err) => {
+    Log.error(err);
+});
